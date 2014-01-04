a0f38e82d5ee4fbd78aa7f8834795a6d396cd2ff
setrar <setrar@valiha.com>
2014-01-04 15:59:07 -0500

    Refactoring a bit

--- src/main/scala/org/egooyiri/EOService.scala
+++ src/main/scala/org/egooyiri/EOService.scala
@@ -7,7 +7,7 @@ import MediaTypes._
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
-class MyServiceActor extends Actor with MyService {
+class EOServiceActor extends Actor with EOService {
 
   // the HttpService trait defines only one abstract member, which
   // connects the services environment to the enclosing actor or test
@@ -16,21 +16,21 @@ class MyServiceActor extends Actor with MyService {
   // this actor only runs our route, but you could add
   // other things here, like request stream processing
   // or timeout handling
-  def receive = runRoute(myRoute)
+  def receive = runRoute(eoRoute)
 }
 
 
 // this trait defines our service behavior independently from the service actor
-trait MyService extends HttpService {
+trait EOService extends HttpService {
 
-  val myRoute =
+  val eoRoute =
     path("") {
       get {
         respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
           complete {
             <html>
               <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+                <h1>Say Manao Ahaona to <i>Ego Oyiry-routing</i> on <i>spray-can</i>!</h1>
               </body>
             </html>
           }
--- src/test/scala/org/egooyiri/EOServiceSpec.scala
+++ src/test/scala/org/egooyiri/EOServiceSpec.scala
@@ -9,25 +9,25 @@ import org.junit.runner._
 import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class MyServiceSpec extends Specification with Specs2RouteTest with MyService {
+class MyServiceSpec extends Specification with Specs2RouteTest with EOService {
   def actorRefFactory = system
   
   "MyService" should {
 
     "return a greeting for GET requests to the root path" in {
-      Get() ~> myRoute ~> check {
-        responseAs[String] must contain("Say hello")
+      Get() ~> eoRoute ~> check {
+        responseAs[String] must contain("Say Manao Ahaona")
       }
     }
 
     "leave GET requests to other paths unhandled" in {
-      Get("/kermit") ~> myRoute ~> check {
+      Get("/kermit") ~> eoRoute ~> check {
         handled must beFalse
       }
     }
 
     "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(myRoute) ~> check {
+      Put() ~> sealRoute(eoRoute) ~> check {
         status === MethodNotAllowed
         responseAs[String] === "HTTP method not allowed, supported methods: GET"
       }
